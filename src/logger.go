120b243795e5ad165746d60e2a20a35f189a74e5
Miclle <miclle.zheng@gmail.com>
2016-04-14 16:11:33 +0800

    Update logger.GenReqId method

--- src/logger.go
+++ src/logger.go
@@ -13,15 +13,11 @@ import (
 
 var pid = uint32(time.Now().UnixNano() % 4294967291)
 
-func genReqId() string {
+func GenReqId() string {
 	var b [12]byte
 	binary.LittleEndian.PutUint32(b[:], pid)
 	binary.LittleEndian.PutUint64(b[4:], uint64(time.Now().UnixNano()))
 	return base64.URLEncoding.EncodeToString(b[:])
-}
-
-func GenReqId() string {
-	return genReqId()
 }
 
 var (
